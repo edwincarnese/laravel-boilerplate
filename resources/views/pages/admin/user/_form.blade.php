fc32a2c165a470026331b4375676c1d49c9776f0
Edwin Carnese <edwincarnese@gmail.com>
2021-04-02 14:52:25 +0800

    feat: add div ids in user modal form

--- resources/views/pages/admin/user/_form.blade.php
+++ resources/views/pages/admin/user/_form.blade.php
@@ -10,34 +10,34 @@
             <div>
                 <form action="javascript:void(0);" method="POST" role="form" id="form">
                     <div class="card-body">
-                        <div class="row">
+                        <div class="row" id="form-fullname">
                             <div class="form-group col-md-6">
                                 <label>First Name</label>
-                                <input type="text" id="first_name" name="first_name" class="form-control" >
+                                <input type="text" id="first_name" name="first_name" class="form-control">
                             </div>
                             <div class="form-group col-md-6">
                                 <label>Last Name</label>
-                                <input type="text" id="last_name" name="last_name" class="form-control" >
+                                <input type="text" id="last_name" name="last_name" class="form-control">
                             </div>
                         </div>
-                        <div class="row">
+                        <div class="row" id="form-credentials">
                             <div class="form-group col-md-6">
                                 <label>User Name</label>
-                                <input type="text" id="username" name="username" class="form-control" >
+                                <input type="text" id="username" name="username" class="form-control">
                             </div>
                             <div class="form-group col-md-6">
                                 <label>Email Address</label>
-                                <input type="email" id="email" name="email" class="form-control" >
+                                <input type="email" id="email" name="email" class="form-control">
                             </div>
                         </div>
-                        <div class="row">
+                        <div class="row" id="form-passwords">
                             <div class="form-group col-md-6">
                                 <label>Password</label>
-                                <input type="password" id="password" name="password" class="form-control" autocomplete="on" >
+                                <input type="password" id="password" name="password" class="form-control" autocomplete="on">
                             </div>
                             <div class="form-group col-md-6">
                                 <label>Confirm Password</label>
-                                <input type="password" id="password_confirmation" name="password_confirmation" class="form-control" autocomplete="on" >
+                                <input type="password" id="password_confirmation" name="password_confirmation" class="form-control" autocomplete="on">
                             </div>
                         </div>
                     </div>
@@ -46,7 +46,7 @@
             <p id="error-message" class="text-center text-danger"></p>
             <div class="modal-footer justify-content-between">
                 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                <button type="submit" class="btn btn-primary" id="save-form" form="form">Save</button>
+                <button type="submit" class="btn btn-primary" id="submit-form" form="form">Save</button>
             </div>
         </div>
     </div>
