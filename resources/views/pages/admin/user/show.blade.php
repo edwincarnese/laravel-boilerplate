fcef9096a588f89bda556d16503361d73520d288
Edwin Carnese <edwincarnese@gmail.com>
2021-04-15 20:22:57 +0800

    feat: basic information for user

--- resources/views/pages/admin/user/show.blade.php
+++ resources/views/pages/admin/user/show.blade.php
@@ -9,9 +9,30 @@
 
 @section('content')
 <div class="content-wrapper">
-  @include('partials.breadcrumb', ['title' => 'Users'])
+  @include('partials.breadcrumb', ['title' => Auth::user()->full_name ])
   <div class="content">
     <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-3">
+          <div class="card card-primary card-outline">
+            <div class="card-body box-profile">
+              <div class="text-center">
+                <img class="profile-user-img img-fluid img-circle" 
+                  src="../../dist/img/user4-128x128.jpg" 
+                  alt="User profile picture">
+              </div>
+              <h3 class="profile-username text-center">{{ Auth::user()->full_name }}</h3>
+              <p class="text-muted text-center">Software Engineer</p>
+              <ul class="list-group list-group-unbordered mb-3">
+                <li class="list-group-item">
+                  <b>Subscribers</b> <a class="float-right">1,322</a>
+                </li>
+              </ul>
+              <a href="#" class="btn btn-primary btn-block"><b>Follow</b></a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </div>
